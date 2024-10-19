35d2d0e2844b821ce985926c70e21b4f3fa72020
Giulia Moukouyou <giuliadeveloper@users.noreply.github.com>
2024-10-19 11:28:12 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Frontend Mentor - Minimalist portfolio website solution
+
+This is a solution to the [Minimalist portfolio website challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/minimalist-portfolio-website-LMy-ZRyiE). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+
+
+
+## Project Overview
+
+This project is a minimalist portfolio website created to showcase my web development skills. It emphasizes responsive design and dynamic interactivity using HTML5, CSS3, and JavaScript. The website uses a mobile-first approach and incorporates CSS Grid for flexible layout management across different screen sizes. Throughout this project, I focused on best practices for modern front-end development, improving performance and ensuring the website is visually appealing and user-friendly on various devices.
+
+
+## Live website
+
+See the live website [here ](https://minimalist-static-portfolio.netlify.app/).
+
+
+### Screenshot
+
+![](./images/screenshot/Screenshot.png)
+
+
+## Features
+
+1. Responsive Design (Mobile-First)
+
+	•	The website follows a mobile-first design approach, making sure it looks great on small devices first, and then enhancing the layout for larger screens like tablets and desktops.
+	•	Media queries are used to handle breakpoints for tablets (768px) and desktops (1024px), ensuring a smooth transition between devices.
+	•	The layout adapts dynamically using CSS Grid to adjust content positioning depending on the screen width.
+
+2. CSS Grid Layout
+
+	•	CSS Grid is used extensively to create flexible and responsive layouts for the main sections, such as the hero section, portfolio projects, and contact form.
+	•	The grid structure is designed to easily switch between layouts, with grid-template-areas used to rearrange project sections on larger screens, allowing for different column orders (e.g., reversing image and text in the portfolio section).
+
+3. Interactive JavaScript Features
+
+	•	JavaScript is used to dynamically change image sources based on the current screen size, ensuring that the appropriate image is loaded for mobile, tablet, or desktop devices.
+	•	The hamburger menu is implemented for smaller screens, allowing for a collapsible navigation bar. Clicking the menu icon toggles the visibility of the navigation links.
+	•	Smooth scrolling functionality is added to buttons such as “Go to About Me” and “Go to Portfolio”, enhancing the user experience.
+
---
+++ script.js
@@ -0,0 +1,165 @@
+document.addEventListener("DOMContentLoaded", function() {
+/*  ------------------------
+    SHOW/HIDE HAMBURGER MENU 
+    ------------------------ */
+    const HAMBURGER_ICON = document.querySelector(".hamburger-container");
+    const HEADER = document.getElementById("header");
+    const MAIN = document.getElementById("main");
+    const FOOTER = document.getElementById("footer");
+    const LINKS = document.querySelector(".navigation-links");
+
+    if (HAMBURGER_ICON) {
+        HAMBURGER_ICON.addEventListener('click', () => {
+            HAMBURGER_ICON.classList.toggle('active');
+            HEADER.classList.toggle('active');
+            MAIN.classList.toggle('active');
+            FOOTER.classList.toggle('active');
+            LINKS.classList.toggle('active');      
+        });
+    } else {
+        console.error('Hamburger icon not found!');
+    }
+     
+    // set the menu to close when its links are clicked
+    const NAVLINKS = document.querySelectorAll(".navigation-links a");
+    console.log(NAVLINKS);
+    NAVLINKS.forEach(link => {
+        link.addEventListener("click", function() {
+            HAMBURGER_ICON.classList.remove('active');
+            HEADER.classList.remove('active');
+            MAIN.classList.remove('active');
+            FOOTER.classList.remove('active');
+            LINKS.classList.remove('active');
+        });
+    });
+
+    /*  ------------------------------------------------------
+        GO TO ABOUT SECTION ON CLICK TO THE "ABOUT ME" BUTTON
+        ----------------------------------------------------- */
+    
+            const elementsI = document.getElementsByClassName('go-to-about-me');
+                for (let i = 0; i < elementsI.length; i++) {
+                elementsI[i].addEventListener('click', function() {
+                    document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
+                });
+            };
+
+    /*  --------------------------------------------------------------------------------------------
+        GO TO PORTFOLIO SECTION ON CLICK TO THE "GO TO PORTFOLIO" BUTTON
+        -------------------------------------------------------------------------------------------- */
+        
+            const elementsJ = document.getElementsByClassName('see-portfolio');
+                for (let i = 0; i < elementsJ.length; i++) {
+                elementsJ[i].addEventListener('click', function() {
+                    document.getElementById('portfolio').scrollIntoView({ behavior: 'smooth' });
+                });
+            };       
+            
+        /*--------------------------------------------------------------------------------------------
+        CHANGE THE SOURCE OF IMAGES (TABLET / DESKTOP/ DEFAULT MOBILE) DEPENDING ON ON WINDOW WIDTH:
+        --------------------------------------------------------------------------------------------
+        
+        ----------------------------
+        ABOUT ME IMAGE 
+        ---------------------------- */
+            var profile_image = document.querySelector(".profile-image");
+            function replacePortrait() {
+                var windowWidth = window.innerWidth || document.documentElement.clientWidth;
+                
+                if(windowWidth >= 768 && windowWidth < 1024) {
+                    profile_image.src = "./images/homepage/tablet/no-bg.png";
+                    console.clear();
+                    console.log(`profile-image src: ${profile_image.src}`);
+                }
+                else if (windowWidth >= 1024) {
+                    profile_image.src = "./images/homepage/desktop/adobe-busto-nobg.png"; 
+                    console.clear();
+                    console.log(`profile-image src: ${profile_image.src}`);
+                } else {
+                    profile_image.src = " ./images/homepage/mobile/mobile-portrait.png";
+                    console.clear();
+                    console.log(`profile-image src: ${profile_image.src}`);
+                }
+            }
+            
+            replacePortrait();
+            window.addEventListener('resize', replacePortrait);
+            
+            
+        /*  ----------------------------
+        "MANAGE" PROJECT IMAGE 
+        ---------------------------- 
+            var manage_image = document.querySelector(".manage");
+            function replaceManage() {
+                var windowWidth = window.innerWidth || document.documentElement.clientWidth;
+                
+                if(windowWidth >= 768 && windowWidth < 1024) {
+                    manage_image.src = "./images/portfolio/tablet/image-portfolio-manage.jpg";
+                    console.log(`manage-image src: ${manage_image.src}`);
+                }
+                else if (windowWidth >= 1024) {
+                    manage_image.src = "./images/portfolio/desktop/image-portfolio-manage.jpg"; 
+                    console.log(`manage-image src: ${manage_image.src}`);
+                } else {
+                    manage_image.src = " ./images/portfolio/mobile/image-portfolio-manage.jpg";
+                    console.log(`manage-image src: ${manage_image.src}`);
+                }
+            } 
+            
+            replaceManage();
+            window.addEventListener('resize', replaceManage); */
+            
+            
+        /*  ----------------------------
+        "INSURE" PROJECT IMAGE 
+        ---------------------------- */
+            var insure_image = document.querySelector(".insure");
+            function replaceInsure() {
+                var windowWidth = window.innerWidth || document.documentElement.clientWidth;
+                
+                if(windowWidth >= 768 && windowWidth < 1024) {
+                    insure_image.src = "./images/portfolio/tablet/image-portfolio-insure.jpg";
+                    console.log(`insure-image src: ${insure_image.src}`);
+                }
+                else if (windowWidth >= 1024) {
+                    insure_image.src = "./images/portfolio/desktop/image-portfolio-insure.jpg"; 
+                    console.log(`insure-image src: ${insure_image.src}`);
+                } else {
+                    insure_image.src = " ./images/portfolio/mobile/image-portfolio-insure.jpg";
+                    console.log(`insure-image src: ${insure_image.src}`);
+                }
+            }
+            
+            replaceInsure();
+            window.addEventListener('resize', replaceInsure);
+            
+            
+        /*  ----------------------------
+        "BOOKMARK" PROJECT IMAGE 
+        ---------------------------- */
+            var bookmark_image = document.querySelector(".bookmark");
+            function replaceBookmark() {
+                var windowWidth = window.innerWidth || document.documentElement.clientWidth;
+                
+                if(windowWidth >= 768 && windowWidth < 1024) {
+                    bookmark_image.src = "./images/portfolio/tablet/image-portfolio-bookmark.jpg";
+                    console.log(`bookmark-image src: ${bookmark_image.src}`);
+                }
+                else if (windowWidth >= 1024) {
+                    bookmark_image.src = "./images/portfolio/desktop/image-portfolio-bookmark.jpg"; 
+                    console.log(`bookmark-image src: ${bookmark_image.src}`);
+                } else {
+                    bookmark_image.src = " ./images/portfolio/mobile/image-portfolio-bookmark.jpg";
+                    console.log(`bookmark-image src: ${bookmark_image.src}`);
+                }
+            }
+            
+            replaceBookmark();
+            window.addEventListener('resize', replaceBookmark);
+});
+
+
+    
+
+
+
